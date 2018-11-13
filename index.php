44b6c616a68c4376ce5b8c228a9c2d5b8f2d4d43
Ewerton Azevedo <ewerton.azevedo@hotmail.com>
2018-11-13 20:03:55 -0200

    alterando conteudo do

--- index.php
+++ index.php
@@ -24,12 +24,7 @@ $arr = array(
 								),
 	'contato' => array(
 									'telefone' => '1111-2222',
-									'email' => 'ewerton@email.com',
-									'redes' => array(
-																'facebook' => 'fb.com/ewerton',
-																'twitter' => 'tw.com/eweron',
-																'instagran' => 'insta.com/ewerton'
-														 )
+									'email' => 'ewerton@email.com'
 								)
 );
 
